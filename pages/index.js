e8e7ff51c30bd4acdd783d16ecf601f033923fb0
Henrique Kramer <henriquemkramer@gmail.com>
2023-09-05 21:14:41 -0300

    change phrase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Bia, eu amo você. Se você me ama, dá uma risadinha =)</h1>
+  return <h1>Bia, eu amo você. Se você me ama, dá uma risadinha! 🥰🥰</h1>
 }
 
 export default Home;
