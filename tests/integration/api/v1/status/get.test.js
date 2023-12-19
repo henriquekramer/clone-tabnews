3e59acb886d3086e9cb213e8eef9cdecdbf82082
Henrique Kramer <henriquemkramer@gmail.com>
2023-12-19 00:22:34 -0300

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,12 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  const dbOpenedConnections =
+    responseBody.dependecies.database.opened_connections;
 
   expect(response.status).toBe(200);
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.dependecies.database.version).toEqual("16.0");
+  expect(dbOpenedConnections).toEqual(1);
 });
