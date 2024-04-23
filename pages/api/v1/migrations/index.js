d03455feb85d7d7e1a63732f2ab61fc0c65f9ebc
Henrique Kramer <henriquemkramer@gmail.com>
2024-04-22 21:55:14 -0300

    remove space

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -31,7 +31,6 @@ export default async function migrations(request, response) {
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
     }
-
     return response.status(200).json(migratedMigrations);
   }
 
