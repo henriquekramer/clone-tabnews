26f8385c48613dbb8d96a8502abc7a4d6ff18997
Henrique Kramer <henriquemkramer@gmail.com>
2024-04-24 22:55:50 -0300

    fix: `/migration` endpoint connection bug

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,10 +3,10 @@ test("GET to /api/v1/status should return 200", async () => {
   const responseBody = await response.json();
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   const dbOpenedConnections =
-    responseBody.dependecies.database.opened_connections;
+    responseBody.dependencies.database.opened_connections;
 
   expect(response.status).toBe(200);
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-  expect(responseBody.dependecies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
   expect(dbOpenedConnections).toEqual(1);
 });
