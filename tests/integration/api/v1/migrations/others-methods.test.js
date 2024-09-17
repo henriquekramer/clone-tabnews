0cabe77363faad885e0027df2e9e60d1a1953141
Henrique Kramer <henriquemkramer@gmail.com>
2024-09-16 22:50:57 -0300

    refactor: use orchestrator.clearDatabase() in tests

--- tests/integration/api/v1/migrations/others-methods.test.js
+++ tests/integration/api/v1/migrations/others-methods.test.js
@@ -1,14 +1,9 @@
-import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await cleanDatabase();
+  await orchestrator.clearDatabase();
 });
-
-async function cleanDatabase() {
-  await database.query("drop schema public cascade; create schema public;");
-}
 
 async function getMigrationsResponse(method) {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
@@ -26,7 +21,7 @@ describe("OTHER HTTP METHODS /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     test("Ensuring there are no open connections to the database", async () => {
       for (let method of ["HEAD", "PUT", "DELETE", "OPTIONS", "PATCH"]) {
-        await cleanDatabase();
+        await orchestrator.clearDatabase();
 
         const migrationsResponse = await getMigrationsResponse(method);
         expect(migrationsResponse.status).toBe(405);
